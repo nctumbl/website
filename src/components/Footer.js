f3f8687993d58e59f83b79ab12934fd130a85aa3
kuragechan14 <kuragechan14@gmail.com>
2018-12-20 14:23:55 +0800

    Update the footer info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = styled.footer`
 export default () => (
   <Footer className="bg-light">
     <p>Copyright © 2018 MBL All Rights Reserved.</p>
-    <p>國立交通大學 資訊工程系 行動計算與寬頻網路實驗室 工程三館235室</p>
-    <p>電話: (03)5712121 分機: 56653</p>
+    <p>國立交通大學 資訊工程系 行動計算與寬頻網路實驗室 工程三館612室</p>
+    <p>電話: (03)5712121 分機: 54790</p>
   </Footer>
 )
